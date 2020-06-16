dc6d3b4571dc0339e4b0b97f7925e38e9bce465b
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-16 14:38:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,12 @@
 def join_nested_strings(src)
   row_index=0
-  nested_strings=" "
+  nested_strings=[]
   while row_index < src.count do
     inner_index=0
    
     while inner_index< src[row_index].count do
       if src[row_index][inner_index].class == String
-         nested_strings << #{src[row_index][inner_index] } 
+         nested_strings << src[row_index][inner_index] + " "
       end
       inner_index += 1
     end
